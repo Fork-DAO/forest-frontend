817fed4f45f7e63646e76fc10befd5e34543f50b
masch <the.masch@gmail.com>
2023-03-09 20:03:18 -0300

    Calculate total mint price

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Web3 from 'web3';
 
-const contractAddress = "0xCd7Ed93D4b95c9F93Ca7e2b4B8960Bd577A0DF9C";
+const contractAddress = "0x466b832D391cD52E066A8fFc88c39a1fa4547112";
 const abi = [{"inputs":[{"internalType":"uint256","name":"_maxSupply","type":"uint256"},{"internalType":"uint256","name":"_unitPrice","type":"uint256"},{"internalType":"address payable","name":"_treasury","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ConstructorParamsInvalid","type":"error"},{"inputs":[{"internalType":"uint256","name":"_maxSupply","type":"uint256"}],"name":"MaxSupplyReached","type":"error"},{"inputs":[{"internalType":"uint256","name":"_expectedPaymentAmount","type":"uint256"},{"internalType":"uint256","name":"_actualPaymentAmount","type":"uint256"}],"name":"PaymentAmountInvalid","type":"error"},{"inputs":[],"name":"PaymentFailed","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_buyer","type":"address"},{"indexed":false,"internalType":"uint256","name":"_mintQuantity","type":"uint256"}],"name":"PreMint","type":"event"},{"inputs":[{"internalType":"uint256","name":"_mintQuantity","type":"uint256"}],"name":"preMint","outputs":[],"stateMutability":"payable","type":"function"}] // Paste your ABI here
 
 function PreMint(props) {
@@ -36,14 +36,16 @@ function Index() {
   }, [address, web3, contract])
 
   function preMint() {
-    let price = 1;
-    let encoded = contract.methods.preMint(1).encodeABI()
+    const price = 1;
+    const mintAmount = 1;
+    const encoded = contract.methods.preMint(mintAmount).encodeABI()
+    const totalPrice = price * mintAmount;
 
     let tx = {
         from: address,
         to : contractAddress,
         data : encoded,
-        value: web3.utils.numberToHex(price)
+        value: web3.utils.numberToHex(totalPrice)
     }
 
     let txHash = ethereum.request({
