5bec62153c984f3dd19fa12ba6f9f918c2825837
masch <the.masch@gmail.com>
2023-03-08 22:16:53 -0300

    Add premint button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,49 @@
 import React, {useState, useEffect} from 'react';
-let Web3 = require('web3');
+import Web3 from 'web3';
+
+const contractAddress = "0xCd7Ed93D4b95c9F93Ca7e2b4B8960Bd577A0DF9C";
+const abi = [{"inputs":[{"internalType":"uint256","name":"_maxSupply","type":"uint256"},{"internalType":"uint256","name":"_unitPrice","type":"uint256"},{"internalType":"address payable","name":"_treasury","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ConstructorParamsInvalid","type":"error"},{"inputs":[{"internalType":"uint256","name":"_maxSupply","type":"uint256"}],"name":"MaxSupplyReached","type":"error"},{"inputs":[{"internalType":"uint256","name":"_expectedPaymentAmount","type":"uint256"},{"internalType":"uint256","name":"_actualPaymentAmount","type":"uint256"}],"name":"PaymentAmountInvalid","type":"error"},{"inputs":[],"name":"PaymentFailed","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_buyer","type":"address"},{"indexed":false,"internalType":"uint256","name":"_mintQuantity","type":"uint256"}],"name":"PreMint","type":"event"},{"inputs":[{"internalType":"uint256","name":"_mintQuantity","type":"uint256"}],"name":"preMint","outputs":[],"stateMutability":"payable","type":"function"}] // Paste your ABI here
+
+function PreMint(props) {
+  return (
+    <div>
+      <p>{props.address}</p>
+      <button onClick={props.onClick}>
+        PreMint
+      </button>
+    </div>
+  );
+}
 
 function Index() {
 
-  const [web3, setWeb3] = useState(null)
-  const [address, setAddress] = useState(null)
-  const [contract, setContract] = useState(null)
-
-  let abi =[{"inputs":[{"internalType":"uint256","name":"_maxSupply","type":"uint256"},{"internalType":"uint256","name":"_unitPrice","type":"uint256"},{"internalType":"address payable","name":"_treasury","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ConstructorParamsInvalid","type":"error"},{"inputs":[{"internalType":"uint256","name":"_maxSupply","type":"uint256"}],"name":"MaxSupplyReached","type":"error"},{"inputs":[{"internalType":"uint256","name":"_expectedPaymentAmount","type":"uint256"},{"internalType":"uint256","name":"_actualPaymentAmount","type":"uint256"}],"name":"PaymentAmountInvalid","type":"error"},{"inputs":[],"name":"PaymentFailed","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"_buyer","type":"address"},{"indexed":false,"internalType":"uint256","name":"_mintQuantity","type":"uint256"}],"name":"PreMint","type":"event"},{"inputs":[{"internalType":"uint256","name":"_mintQuantity","type":"uint256"}],"name":"preMint","outputs":[],"stateMutability":"payable","type":"function"}] // Paste your ABI here
-  let contractAddress = "0xCd7Ed93D4b95c9F93Ca7e2b4B8960Bd577A0DF9C";
+  const [web3, setWeb3] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [contract, setContract] = useState(null);
 
   useEffect(() => {
     window.ethereum ?
       ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
         setAddress(accounts[0])
-        let w3 = new Web3(ethereum)
+        const w3 = new Web3(ethereum)
         setWeb3(w3)
 
-        let c = new w3.eth.Contract(abi, contractAddress)
+        const c = new w3.eth.Contract(abi, contractAddress)
         setContract(c)
 
-        preMint()
-
-      }).catch((err) => console.log(err))
+      }).catch((err : Error) => console.log(err))
     : console.log("Please install MetaMask")
-  }, [address])
+  }, [address, web3, contract])
 
-  function preMint(){
-    let _price = web3.utils.toWei("0.000000000000000001");
-    let encoded = contract.methods.preMint(2).encodeABI()
+  function preMint() {
+    let price = 1;
+    let encoded = contract.methods.preMint(1).encodeABI()
 
     let tx = {
         from: address,
         to : contractAddress,
         data : encoded,
-        nonce: "0x00",
-        value: web3.utils.numberToHex(_price)
+        value: web3.utils.numberToHex(price)
     }
 
     let txHash = ethereum.request({
@@ -43,16 +51,16 @@ function Index() {
         params: [tx],
     }).then((hash) => {
         alert("You can now view your transaction with hash: " + hash)
-    }).catch((err) => console.log(err))
+    }).catch((err : Error) => console.log(err))
     
     return txHash
   }
 
-    return (
-      <div>
-        Address: {address}
-      </div>
-    )
+  return (
+    <div>
+      {address ? <PreMint address={address} onClick={preMint} /> : <h1> You need to connect your wallet in order to premint the NFT</h1> }
+    </div>
+  )
 }
 
 export default Index
